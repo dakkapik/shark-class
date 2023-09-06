643d385e9e4a916fe7cfd42bd4544aadd167e955
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-09-06 10:03:32 -0400

    first class

---
+++ main.py
@@ -0,0 +1,47 @@
+import pygame
+
+import sys
+
+import os
+
+from pygame.locals import *
+
+
+pygame.init()  # initialize pygame
+
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((1000, 800)) 
+
+
+# Load the background image here. Make sure the file exists!
+
+bg = pygame.image.load(os.path.join("./", "background.png"))
+ball = pygame.image.load(os.path.join("./", "ball.png"))
+
+pygame.mouse.set_visible(0)
+
+pygame.display.set_caption('Space Age Game')
+
+
+# fix indentation
+
+
+while True:
+
+    clock.tick(60)
+
+    screen.blit(bg, (0, 0))
+    screen.blit(ball, (300,300))
+
+    x, y = pygame.mouse.get_pos()
+
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+
+            sys.exit()
+
+
+    pygame.display.update()
