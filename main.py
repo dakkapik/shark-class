0ee05430b3477e3112ee845f91b58bc14c04a3d2
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-09-28 09:37:40 -0400

    completed movement module
    - added DIR folder
    -added DIR to path.join
    -added key assignment fn to entity
    -added key table relation for relating inputs to entity changes

--- main.py
+++ main.py
@@ -3,18 +3,50 @@ import sys
 import os
 from pygame.locals import *
 
+ASSETS_DIR = './assets'
+
 SCREEN_SIZE = (1000,600)
 FLOOR_POSITION = 550
 
+keyPressed = ({
+    'd':False,
+    'a':False
+})
+
+
+class Game () :
+    def __init__(self, imagePath = False, size = (1000,600)) -> None:
+        
+        if(imagePath):
+            img = pygame.image.load(os.path.join(ASSETS_DIR, imagePath))
+            self.img = pygame.transform.scale(img, size)
+        else:
+            self.img = False
+        
+        
+    def draw(self):
+        if(self.img):
+            self.screen.blit(self.img, (0,0))
+        else:
+            screen.fill("white")
+        pass
+    
+    def update(self):
+        self.draw()
+        pass
+        
 class Entity () :
     def __init__(self, imagePath, size, screen) -> None:
 
-        img = pygame.image.load(os.path.join("./", imagePath))
+        img = pygame.image.load(os.path.join(ASSETS_DIR, imagePath))
         self.img = pygame.transform.scale(img, size)
 
         self.screen = screen
         self.pos = [SCREEN_SIZE[0]/2, 0]
         self.velocity = [0,0]
+        
+        self.inputsActive = False
+        self.inputs = {}
 
         pass
     
@@ -33,6 +65,22 @@ class Entity () :
             self.screen.blit(self.img, (self.pos[0],self.pos[1]))
             pass
     
+    def checkInputs(self):
+        self.velocity[0] = 0
+        if(keyPressed[self.inputs['right']]):
+            self.velocity[0] = 5
+            
+        if(keyPressed[self.inputs['left']]):
+            self.velocity[0] = -5
+            
+    def setRightKey(self, key):
+        self.inputsActive = True
+        self.inputs['right'] = key
+    
+    def setLeftKey(self, key):
+        self.inputsActive = True
+        self.inputs['left'] = key
+    
     def update( self ):
         self.pos[0] = self.pos[0] + self.velocity[0]
         self.pos[1] = self.pos[1] + self.velocity[1]
@@ -43,6 +91,9 @@ class Entity () :
             self.velocity[1] = self.velocity[1] + 0.08
         else:
             self.velocity[1] = 0
+
+        if(self.inputsActive):
+            self.checkInputs()
 
         self.draw()
         pass
@@ -57,15 +108,16 @@ screen = pygame.display.set_mode(SCREEN_SIZE)
 
 # Load the background image here. Make sure the file exists!
 
-bg = pygame.image.load(os.path.join("./", "background.png"))
-
+# bg = pygame.image.load(os.path.join("./", "background.png"))
+game = Game()
 
 ball_1 = Entity("ball.png", (50,50), screen)
-
-
-
 ball_2 = Entity("ball.png", (50,50), screen)
 ball_3 = Entity("ball.png", (50,50), screen)
+
+ball_1.setRightKey('d')
+ball_1.setLeftKey('a')
+
 
 ball_2.changePosition([100,0])
 ball_3.changePosition([300,0])
@@ -84,9 +136,7 @@ while (True):
 
     clock.tick(60)
 
-    screen.blit(bg, (0, 0))
-
-
+    game.update()
     ball_1.update()
 
     ball_2.update()
@@ -97,10 +147,19 @@ while (True):
         if event.type == pygame.KEYDOWN:
 
             if(event.key == pygame.K_w):
-                
                 ball_1.jump()
-
+            if(event.key == pygame.K_d):
+                keyPressed['d'] = True
+            if(event.key == pygame.K_a):
+                keyPressed['a'] = True
+            
             print(event.key)
+
+        if(event.type == pygame.KEYUP):
+            if(event.key == pygame.K_d):
+                keyPressed['d'] = False
+            if(event.key == pygame.K_a):
+                keyPressed['a'] = False
 
         if event.type == pygame.QUIT:
 
